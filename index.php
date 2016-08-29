d88a5b58b8db0d8bf3010edaffd104da5137163e
MustafaTaj <for.u.400@gmail.com>
2016-08-30 02:54:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -65,8 +65,7 @@ try
     {
        $responses = $client->sendChatAction(['chat_id' => $update->message->
             chat->id, 'action' => 'typing']);
-            
-        CurlRequest2($MsgSend, false);
+        $response = CurlRequest2($update->message->text);    
         if (empty($response["response"]))
             $response["response"] = "سيبني حالياً, أنا زعلان وعاوز أقعد براي";
 
@@ -76,8 +75,7 @@ try
 
         $MsgSend = "[" . $update->message->chat->username . "]: " . $update->
             message->text. "xzxz [Response]: " . $response["response"];
-            
-        $response = CurlRequest2($update->message->text);
+        CurlRequest2($MsgSend, false);    
         $client->sendMessage(['chat_id' => $update->message->chat->id, 'text' =>
             $response["response"]]);
     }
